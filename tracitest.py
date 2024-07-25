fe509f26f5c5d465a761d52314825fbc95e2220f
Jonas Speiser <spj1wi@bosch.com>
2024-07-25 14:46:13 +0200

    vehicle reroutes to charge when soc falls under certain value

--- tracitest.py
+++ tracitest.py
@@ -10,24 +10,42 @@ def add_vehicles():
     traci.route.add("trip", ["E0", "E3"])
     traci.vehicle.add("myVehicle", "trip", typeID="DEFAULT_VEHTYPE")
 
+def reroute_for_charging(vehicle_id, cs_id):
+    """reroute vehicle via charging station"""
+    v_type = traci.vehicle.getTypeID(vehicle_id)
+    vehicle_lane = traci.vehicle.getLaneID(vehicle_id)
+    current_vehicle_edge = traci.lane.getEdgeID(vehicle_lane)
+    destination = traci.vehicle.getRoute(vehicle_id)[-1]
+    cs_lane = traci.chargingstation.getLaneID(cs_id)
+    cs_edge = traci.lane.getEdgeID(cs_lane)
+
+    route_to_cs = traci.simulation.findRoute(current_vehicle_edge, cs_edge, v_type)
+    route_from_cs = traci.simulation.findRoute(cs_edge, destination, v_type)
+    new_route = route_to_cs.edges + route_from_cs.edges[1:]
+    traci.vehicle.setRoute(vehicle_id, new_route)
+    traci.vehicle.setChargingStationStop(vehicle_id, cs_id, duration=100)
+
+
 def run_simulation():
-    step = 0
-    while traci.simulation.getMinExpectedNumber() > 0: # while there are cars in the simulation
+    """execute the TraCI control loop"""
+    # main loop: while there are cars in the simulation
+    while traci.simulation.getMinExpectedNumber() > 0: 
         try:
-            battery_soc = traci.vehicle.getParameter("myVehicle", "device.battery.actualBatteryCapacity")
+            vehicle_id = "myVehicle"
+            battery_soc = traci.vehicle.getParameter(vehicle_id, "device.battery.actualBatteryCapacity")
             print("battery capacity: ", battery_soc)
+            if float(battery_soc) < 17450:
+                print("Battery low, rerouting to charge")
+                cs_id = "cs_1"
+                reroute_for_charging(vehicle_id, cs_id)
         except traci.TraCIException as e:
             print(e)
-        step += 1
+        traci.simulationStep()
 
     traci.close()
 
 
     """
-    TODO: Charging Station available
-    TODO: Tell car to reroute to a certain position
-    TODO: Tell car to charge at said position
-    TODO: Tell car to reroute to destination
     TODO: Use network file from eAlloc
     """
 
@@ -40,6 +58,7 @@ if __name__ == "__main__":
         '--start', # start simulation immediately
         '--device.battery.probability', '1' # sets all vehicles to be EVs instead of combustion engine
         ]
+
     traci.start(sumoCmd)
 
     add_vehicles()
